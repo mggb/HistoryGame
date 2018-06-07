a9252ba1ac79617f0909632d3661377a9acb705d
mggb <mathieu.gelbon@hetic.net>
2018-06-07 02:24:24 +0200

    'a'

---
+++ data.js
@@ -0,0 +1,75 @@
+var data = {
+  GoodPlace:[{
+    color: 'violet',
+    question: 'Ou habiteriez vous dans << The good place >> ?',
+    reponse1: 'un manoir gothique',
+    reponse2: 'un trou noir',
+    reponse3: 'un apartement minimaliste',
+    reponse4: 'un chateau luxieux',
+    reponse5: 'une maiso au bord de la plage',
+    reponse6: 'une bibliotheque'
+  },{
+    color: 'rose',
+    question: 'Que trouverait-on dans votre << The good place >> ?',
+    reponse1: 'des livres',
+    reponse2: 'du bon vin',
+    reponse3: 'des fringue stylées',
+    reponse4: 'des girafes',
+    reponse5: 'des gros mots',
+    reponse6: 'tout les jeux de x-box'
+  },{
+    color: 'orange',
+    question: 'Si vous ne deviez manger qu’un seul aliment pour le reste de vos jours ?',
+    reponse1: 'des fruits de mer',
+    reponse2: 'des burger',
+    reponse3: 'des chatons',
+    reponse4: 'des fraise',
+    reponse5: 'de la truffe',
+    reponse6: 'e ne peux pas en choisir qu’un, c’est impossible'
+  },{
+    color: 'jaune',
+    question: 'Qui serait votre âme soeur dans « the good place » ?',
+    reponse1: 'séléna Gomez',
+    reponse2: 'un ballon',
+    reponse3: 'celine dion',
+    reponse4: 'Kanye West',
+    reponse5: 'Brad Pitt',
+    reponse6: 'une bibliotheque'
+  },{
+    color: 'vert',
+    question: 'Qu’est ce qui est le plus important pour vous ?',
+    reponse1: 'l inteligence',
+    reponse2: 'le bohneur',
+    reponse3: 'largent',
+    reponse4: 'l amour',
+    reponse5: 'le pouvoir',
+    reponse6: 'l amitié'
+  },{
+    color: 'bleu',
+    question: 'Quel est votre pire péché ?',
+    reponse1: 'je ne recycle pas',
+    reponse2: 'je like tout sans regarder sur tinder',
+    reponse3: 'je prend en photo tous mes repas pour instagram',
+    reponse4: 'je suis incapable de garder un secret',
+    reponse5: 'je suis de NRJ12',
+    reponse6: 'je juge les gens'
+  },{
+    color: 'yellow',
+    question: 'Choisissez un parfun de glace :',
+    reponse1: 'pistache',
+    reponse2: 'vanille',
+    reponse3: 'chocolat',
+    reponse4: 'cerise',
+    reponse5: 'caramel beurre salé',
+    reponse6: 'fraise des bois'
+  },{
+    color: 'rouge',
+    question: 'Quel est votre plus grande qualité ?',
+    reponse1: 'je suis inteligente',
+    reponse2: 'je suis drole',
+    reponse3: 'je suis honette',
+    reponse4: 'je suis sympa',
+    reponse5: 'embition',
+    reponse6: 'h24 raison'
+  }, ]
+};
--- script.js
+++ script.js
@@ -1,48 +1,100 @@
 var changer = document.querySelector('.mainContainer');
+var opening = document.querySelector('.yolo');
 var starting = document.querySelector('.container1');
+var pi = document.querySelector('.section');
+var finish = document.querySelectorAll('.finish');
+var answers = ["reponse1", "reponse2", "reponse3", "reponse4", "reponse5", "reponse6"];
+var wesh = [];
 
 
+var stockage = [];
+
+opening.addEventListener('click', function() {
+  opening.style.display = "none";
+  changer.style.display = "block";
+});
 starting.addEventListener('click', function() {
   changer.style.display = "none";
 });
 
+function construction(i) {
+  var section = document.querySelector('.section');
+  var content = "<div class='container'>";
+  content += "<div class='question'>";
+  content += "<p class='textinanswer'>";
+  content += data.GoodPlace[i].question;
+  content += "</p>";
+  content += "</div>";
+  content += "<div class='containerAwswer'>";
+  content += "<ul class='ul'>";
+  content += "<li class='awswer'>";
+  content += "<p class='textinanswer'data-key=1>";
+  content += data.GoodPlace[i].reponse1;
+  content += "</p>";
+  content += "</li>"
+  content += "<li class='awswer'>";
+  content += "<p class='textinanswer'data-key=2>";
+  content += data.GoodPlace[i].reponse2;
+  content += "</p>";
+  content += "</li>";
+  content += "<li class='awswer'>";
+  content += "<p class='textinanswer'data-key=3>";
+  content += data.GoodPlace[i].reponse3;
+  content += "</p>";
+  content += "</li>"
+  content += "</ul>"
+  content += "<ul class='ul'>";
+  content += "<li class='awswer'>";
+  content += "<p class='textinanswer'data-key=4>";
+  content += data.GoodPlace[i].reponse4;
+  content += "</p>";
+  content += "</li>";
+  content += "<li class='awswer'>";
+  content += "<p class='textinanswer'data-key=5>";
+  content += data.GoodPlace[i].reponse5;
+  content += "</p>";
+  content += "</li>";
+  content += "<li class='awswer'>";
+  content += "<p class='textinanswer'data-key=6>";
+  content += data.GoodPlace[i].reponse6;
+  content += "</p>";
+  content += "</li>";
+  content += "</ul>";
+  content += "</div>";
+  content += "</div>";
+  section.innerHTML = content;
+  return section
+}
 
-// var chrono = {
-//   secondsLeft: 0,
-//   timer: undefined,
+var wesh = '';
 
-  // Start: function(secondsLeft) {
-  //   //Initialisation du nombre de secondes selon la valeur passée en paramètre
-  //   this.secondsLeft = secondsLeft;
-  //   //Démarrage du chrono
-  //   this.timer = setInterval(this.Tick.bind(this), 1000);
-  // },
+function recupRep() {
+  var global;
+  var section = document.querySelector('.section');
+  var reponseGarde = document.querySelectorAll('.textinanswer');
+  for (var i = 0; i < reponseGarde.length; i++) {
+    reponseGarde[i].addEventListener('click', function(element) {
+      wesh = "reponse" + this.getAttribute('data-key');
+      console.log("1 :", wesh)
+      stockage.push(wesh);
+      pi.innerHTML = ' ';
+    });
+  }
+  return wesh
+}
+function finish() {
 
-//   Tick: function() {
-//     //On actualise la valeur affichée du nombre de secondes
-//     document.getElementById("time").innerHTML = --this.secondsLeft;
-//     if (this.secondsLeft === 0)
-//       //Tps écoulé -> arrêt du timer
-//       this.Stop()
-//   },
-//
-//   Stop: function() {
-//     //quand le temps est écoulé, on arrête le timer
-//     clearInterval(this.timer);
-//     //Et on appelle la fonction qui gère la fin du temps imparti et poursuit le traitement
-//     //Ici, pour le test, simplement une fonction alert
-//     alert('Vous avez perdu');
-//   }
-//
-// };
+}
 
-var x = 6;
-var y = 14;
+
+
+var x = 11;
+var y = 0;
 var victoire = document.querySelector('.victory');
 var table = document.querySelectorAll('tr');
 var colone = document.querySelector('.flower');
-
 var actuel = table[y].children[x];
+
 actuel.classList.add('bonjour');
 console.log(table[y].children[x - 1].classList.contains('test'));
 window.addEventListener('keydown', function(event) {
@@ -59,6 +111,24 @@ window.addEventListener('keydown', function(event) {
     if (left.classList.contains('rip')) {
       victoire.style.display = 'inline';
     }
+    if (left.classList.contains('point')) {
+      console.log(left.dataset.couleur);
+      for (var i = 0; i < data.GoodPlace.length - 1; i++) {
+        if (left.dataset.couleur === data.GoodPlace[i].color) {
+          construction(i);
+          recupRep();
+        }
+      }
+      delete left.dataset.couleur;
+      left.classList.remove('point');
+      left.classList.add('flower');
+
+      if (left.classList.contains('flo')) {
+        console.log('yolo');
+      }
+    }
+    //  var lol = recupRep();
+    console.log(wesh);
   }
   if (event.which == 39) {
     var right = table[y].children[x + 1];
@@ -71,6 +141,22 @@ window.addEventListener('keydown', function(event) {
     if (right.classList.contains('rip')) {
       victoire.style.display = 'inline';
     }
+    if (right.classList.contains('point')) {
+      console.log(right.dataset.couleur);
+      for (var i = 0; i < data.GoodPlace.length - 1; i++) {
+        if (right.dataset.couleur === data.GoodPlace[i].color) {
+          construction(i);
+          recupRep();
+        }
+      }
+      delete right.dataset.couleur;
+      right.classList.remove('point');
+      right.classList.add('flower');
+    }
+    if (right.classList.contains('flo')) {
+      console.log('yolo');
+    }
+    console.log(wesh);
   }
   if (event.which == 38) {
     var top = table[y - 1].children[x];
@@ -80,6 +166,23 @@ window.addEventListener('keydown', function(event) {
       y--;
       actuel = top;
     }
+    if (top.classList.contains('point')) {
+      console.log(top.dataset.couleur);
+      for (var i = 0; i < data.GoodPlace.length - 1; i++) {
+        if (top.dataset.couleur === data.GoodPlace[i].color) {
+          construction(i);
+          recupRep();
+        }
+      }
+      delete top.dataset.couleur;
+      top.classList.remove('point');
+      top.classList.add('flower');
+
+    }
+    if (top.classList.contains('flo')) {
+      console.log('yolo');
+    }
+    console.log(wesh);
   }
   if (event.which == 40) {
     var bot = table[y + 1].children[x];
@@ -92,12 +195,85 @@ window.addEventListener('keydown', function(event) {
     if (bot.classList.contains('rip')) {
       victoire.style.display = 'inline';
     }
+    if (bot.classList.contains('point')) {
+      console.log(bot.dataset.couleur);
+      for (var i = 0; i < data.GoodPlace.length - 1; i++) {
+        if (bot.dataset.couleur === data.GoodPlace[i].color) {
+          construction(i);
+          recupRep();
+        }
+      }
+      delete bot.dataset.couleur;
+      bot.classList.remove('point');
+      bot.classList.add('flower');
+    }
+    if (bot.classList.contains('flo')) {
+      var max;
+      var tmp = 0;
+      for (let i = 0; i < 6; i++) {
+        var count = stockage.filter(word => word === answers[i]);
+
+        if (count.length > tmp) {
+          tmp = count.length;
+          max = i;
+          if (max === 0) {
+            console.log('mon projet est finiiiiiiiiiiiiiiiiiiii');
+            changer.innerHTML=' ';
+            changer.style.display = "block";
+              changer.classList.remove("mainContainer");
+            changer.classList.add("carte1");
+          }
+          if (max === 1) {
+            console.log('mon projet est finiiiiiiiiiiiiiiiiiiii');
+            changer.innerHTML=' ';
+            changer.style.display = "block";
+              changer.classList.remove("mainContainer");
+            changer.classList.add("carte2");
+          }
+          if (max === 2) {
+            console.log('mon projet est finiiiiiiiiiiiiiiiiiiii');
+            changer.innerHTML=' ';
+            changer.style.display = "block";
+              changer.classList.remove("mainContainer");
+            changer.classList.add("carte3");
+          }
+          if (max === 3) {
+            console.log('mon projet est finiiiiiiiiiiiiiiiiiiii');
+            changer.innerHTML=' ';
+            changer.style.display = "block";
+              changer.classList.remove("mainContainer");
+            changer.classList.add("carte4");
+          }
+          if (max === 4) {
+            console.log('mon projet est finiiiiiiiiiiiiiiiiiiii');
+            changer.innerHTML=' ';
+            changer.style.display = "block";
+              changer.classList.remove("mainContainer");
+            changer.classList.add("carte5");
+          }
+          if (max === 5) {
+            console.log('mon projet est finiiiiiiiiiiiiiiiiiiii');
+            changer.innerHTML=' ';
+            changer.style.display = "block";
+            changer.classList.remove("mainContainer");
+            changer.classList.add("carte6");
+          }
+        }
+      }
+    }
+    console.log(wesh);
   }
 
   if (bot !== null && pageYOffset != actuel.offsetTop) {
     console.log('posé');
-    window.scrollTo(0,actuel.offsetTop-400);
+    window.scrollTo(0, actuel.offsetTop - 400);
   }
-
+  if (stockage.length === 7) {
+    console.log('bonjoour jai fini');
+    for (var i = 0; i < finish.length; i++) {
+      finish[i].classList.remove("black")
+      finish[i].classList.add("flower")
+      finish[3].classList.add("flo")
+    }
+  }
 });
-var 0=true
